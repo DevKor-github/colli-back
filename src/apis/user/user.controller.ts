9d2b4f3628661ea516063ab6f07acbb3aa07c49c
spblue4422 <spblue4422@naver.com>
2024-03-21 16:54:06 +0900

    move and add some user apis

--- src/apis/user/user.controller.ts
+++ src/apis/user/user.controller.ts
@@ -3,32 +3,65 @@ import { UserService } from './user.service';
 import { GetProfileResDto } from './dto/getProfileRes.dto';
 import { ModifyProfileReqDto } from './dto/modifyProfileReq.dto';
 import { MsgResDto } from 'src/common/dto/msgRes.dto';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiOkResponse, ApiTags } from '@nestjs/swagger';
+import { TaskDetailResDto } from '../team/task/dtos/taskDetailRes.dto';
+import { TaskService } from '../team/task/task.service';
 
 @ApiTags('user')
-@Controller('user')
+@Controller()
 export class UserController {
-  constructor(private readonly userService: UserService) {}
+  constructor(
+    private readonly userService: UserService,
+    private readonly taskService: TaskService,
+  ) {}
 
-  @Get('/my/:tokenId')
+  @Get('/my')
   async getMyProfile(
-    @Param('tokenId') tokenId: number,
+    @Param('tokenId') tokenId: number, // 본인 유저id값
   ): Promise<GetProfileResDto> {
     return this.userService.getUserProfile(tokenId);
   }
 
-  @Get('/profile/:userId')
+  @Get('/:userId/profile')
   async getUserProfile(
     @Param('userId') userId: number,
   ): Promise<GetProfileResDto> {
     return this.userService.getUserProfile(userId);
   }
 
-  @Patch('/modify/:userId')
+  @Patch('/profile/modify')
   async modifyProfile(
     @Param('tokenId') tokenId: number,
     @Body() modifyProfileReqDto: ModifyProfileReqDto,
   ): Promise<MsgResDto> {
     return this.userService.modifyUser(tokenId, modifyProfileReqDto);
+  }
+
+  @Get('/urgentTask')
+  @ApiOkResponse({ description: '가장 급한 과제' })
+  async getUrgentTask(userId: number): Promise<TaskDetailResDto> {
+    try {
+      // 가입한 팀의 memberId 배열
+      // const joinList = await this.memberService.getUserBelongingList(userId);
+      return this.taskService.getUrgentTask(userId, new Date());
+    } catch (err) {
+      throw err;
+    }
+  }
+
+  @Get('/urgentSchedule')
+  @ApiOkResponse({ description: '가장 급한 일정' })
+  async getUrgentSchedule() {
+    try {
+    } catch (err) {
+      throw err;
+    }
+  }
+
+  @Get('/notice/listbyteam')
+  @ApiOkResponse({ description: '팀별 공지 목록?' })
+  async getNoticeListByTeam() {
+    try {
+    } catch (err) {}
   }
 }
--- src/apis/user/user.module.ts
+++ src/apis/user/user.module.ts
@@ -3,9 +3,10 @@ import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { UserRepository } from './user.repository';
 import { AuthModule } from '../auth/auth.module';
+import { TaskModule } from '../team/task/task.module';
 
 @Module({
-  imports: [forwardRef(() => AuthModule)],
+  imports: [forwardRef(() => AuthModule), TaskModule],
   controllers: [UserController],
   providers: [UserService, UserRepository],
   exports: [UserService],
